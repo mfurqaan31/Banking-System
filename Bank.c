13fd222c50fc4661e14a37f8393bf6d5648f550a
Mohammed Furqaan <furqaanpatel2004@gmail.com>
2023-10-25 20:57:48 +0530

    Update Bank.c

--- Bank.c
+++ Bank.c
@@ -1,4 +1,4 @@
-// correct version 10
+// correct version 12
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -37,6 +37,7 @@ void checkBalance(const char* username);
 void transferMoney(const char* username);
 void displayloginboard(const char* username);
 void createAccount();
+int isUsernameExists(const char* username);
 void login();
 void loginsu();
 void accountCreated();
@@ -91,6 +92,32 @@ int main()
     getch();
 }
 
+int isUsernameExists(const char* username) 
+{
+    FILE* file = fopen("user_data.txt", "r");
+    if (file == NULL) 
+        return 0; 
+    
+    char line[100];
+
+    while (fgets(line, sizeof(line), file) != NULL) 
+    {
+        if (strstr(line, "Username: ") == line) 
+        {
+            char existingUsername[50];
+            sscanf(line, "Username: %s", existingUsername);
+            if (strcmp(existingUsername, username) == 0) 
+            {
+                fclose(file);
+                return 1; 
+            }
+        }
+    }
+
+    fclose(file);
+    return 0;
+}
+
 void createAccount() 
 {
     struct UserData* newUser = (struct UserData*)malloc(sizeof(struct UserData));
@@ -114,8 +141,19 @@ void createAccount()
     scanf("%s", newUser->adharNumber);
     printf("\nPHONE NUMBER: ");
     scanf("%s", newUser->phoneNumber);
-    printf("\nUSERNAME: ");
-    scanf("%s", newUser->username);
+    
+    do 
+    {
+        printf("\nUSERNAME (50 CHARACTERS MAX): ");
+        scanf("%s", newUser->username);
+
+        if (isUsernameExists(newUser->username)) 
+            printf("Username already exists. Please choose a different one.\n");
+        
+    } 
+    while (isUsernameExists(newUser->username));
+
+
     printf("\nPASSWORD: ");
     scanf("%s", newUser->password);
 
@@ -494,6 +532,8 @@ void logout()
     printf("Sign out successfully..\n");
     printf("Press any key to continue...\n");
     getch();
+    FILE *p=fopen("user_data.txt","w+");
+    fclose(p);
 }
 
 
